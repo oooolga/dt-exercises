08af1d6daf5cbb7ea8f1c83814d43a7b7fd92ce2
Olga Luo <olga.xu823@gmail.com>
2020-10-22 11:46:02 -0400

    commit double controllers model

--- control/exercise_ws/src/lane_control/include/lane_controller/controller.py
+++ control/exercise_ws/src/lane_control/include/lane_controller/controller.py
@@ -1,12 +1,16 @@
 import numpy as np
+from duckietown.dtros import DTParam, ParamType
+from copy import deepcopy
+import sys
 
-
-class PurePursuitLaneController:
+class DummyLaneController:
     """
-    The Lane Controller can be used to compute control commands from pose estimations.
+    The Lane Controller can be used to compute control commands from pose
+    estimations.
 
-    The control commands are in terms of linear and angular velocity (v, omega). The input are errors in the relative
-    pose of the Duckiebot in the current lane.
+    The control commands are in terms of linear and angular velocity (v, omega).
+    The input are errors in the relative pose of the Duckiebot in the current
+    lane.
 
     """
 
@@ -18,8 +22,271 @@ class PurePursuitLaneController:
         """Updates parameters of LaneController object.
 
             Args:
-                parameters (:obj:`dict`): dictionary containing the new parameters for LaneController object.
+                parameters (:obj:`dict`): dictionary containing the new
+                parameters for LaneController object.
         """
         self.parameters = parameters
 
+    def get_car_control(self, **kwargs):
+        """
+        Returns a tuple of newly computed linear velocity and angular velocity
+        """
+        return self.parameters["~v_forward"].value, 0.0
 
+class BasicPIDLaneController(DummyLaneController):
+
+    def __init__(self, parameters):
+
+        # call parent class
+        super(BasicPIDLaneController, self).__init__(parameters)
+
+        # initialize parameters
+        self.update_parameters(parameters)
+        # compute k_d for critially damped if the flag is on
+        if self.parameters["~k_d_critically_damped_flag"]:
+            self.set_k_d_for_critically_damped()
+        # compute d_thres for robutness
+        if self.parameters["~d_threshold_robust_flag"]:
+            self.set_d_threshold_for_robutness()
+
+        # initialize terms for last error and accumulated errors
+        self.prev_err = (0.0, 0.0) #(prev_d_err, prev_theta_err)
+        self.I_err = (0.0, 0.0) #(I_d_err, I_tehta_err)
+
+        # just in case if we want to normalize integral error terms
+        self.time_elapsed = (0.0, 0.0) #(time_elapsed_d, time_elapsed_theta)
+
+        # place holder for PID terms (debugging purposes)
+        self.PID = [0.0]*6
+
+    def _exchange_info(self, controllerB):
+
+        self.prev_err = controllerB.prev_err
+        self.I_err = controllerB.I_err
+
+        # just in case if we want to normalize integral error terms
+        self.time_elapsed = controllerB.time_elapsed
+
+    def update_parameters(self, parameters):
+        '''
+        Update parameters.
+        '''
+        super(BasicPIDLaneController, self).update_parameters(parameters)
+
+        if self.parameters["~k_d_critically_damped_flag"]:
+            self.set_k_d_for_critically_damped()
+        if self.parameters["~d_threshold_robust_flag"]:
+            self.set_d_threshold_for_robutness()
+
+    def get_car_control(self, **kwargs):
+        err = (kwargs["d_err"], kwargs["theta_err"])
+        # compute derivatives
+        derr = self.compute_derivative_error(err)
+        # compute integrals
+        self.compute_and_set_I_error(err, kwargs["dt"],
+                                     kwargs["wheels_cmd_flag"])
+        # get constant forward moving speed and angular velocity
+        RET = self.parameters["~v_forward"].value, \
+              self.compute_angular_velocity(err, derr)
+        self.set_prev_err(err)
+        return RET
+
+    def set_k_d_for_critically_damped(self):
+        '''
+        Calculate k_d's value based on k_theta and v_forward that satisfies the
+        critically damped condition.
+        '''
+        k_d = -(self.parameters["~k_theta"].value ** 2 / \
+               (4 * self.parameters["~v_forward"].value))
+        self.parameters["~k_d"].set_value(k_d)
+
+    def set_d_threshold_for_robutness(self):
+        '''
+        '''
+        d_thres = abs(self.parameters["~k_theta"].value * \
+                      self.parameters["~theta_threshold"] / \
+                      self.parameters["~k_d"].value)
+        self.parameters["~d_threshold"] = d_thres
+
+    def compute_derivative_error(self, err):
+        '''
+        Compute the change in error terms.
+        '''
+        return err[0]-self.prev_err[0], err[1]-self.prev_err[1]
+
+    def compute_and_set_I_error(self, err, dt, wheels_cmd_flag):
+        if dt:
+            self.I_err = self.I_err[0] + err[0]*dt, \
+                         self.I_err[1] + err[1]*dt
+            self.time_elapsed = self.time_elapsed[0]+dt, self.time_elapsed[1]+dt
+            self.reset_I_err(err=err, wheels_cmd_flag=wheels_cmd_flag,
+                             mode="if-needed")
+
+    def set_prev_err(self, err):
+        self.prev_err = err
+
+    def reset_I_err(self, err=None, wheels_cmd_flag=None, mode="force-reset"):
+        '''
+        Force reset integral error terms or reset depending on conditions.
+        '''
+        assert mode in ["force-reset", "if-needed"]
+
+        if mode == "hard-reset":
+            self.I_err = (0.0, 0.0)
+            self.time_elapsed = (0.0, 0.0)
+        elif mode == "if-needed":
+            assert err is not None
+            assert wheels_cmd_flag is not None
+            if not wheels_cmd_flag[0] and not wheels_cmd_flag[1]:
+                self.I_err, self.time_elapsed = (0.0, 0.0), (0.0, 0.0)
+                return
+            # we reset integral error terms if current error is negligible
+            # or there is change in direction of the errors
+            if abs(err[0]) < self.parameters["~d_resolution"] or \
+               np.sign(err[0]) != np.sign(self.prev_err[0]):
+                self.I_err, self.time_elapsed = (0.0, self.I_err[1]), \
+                                                (0.0, self.time_elapsed[1])
+            if abs(err[1]) < self.parameters["~theta_resolution"] or \
+               np.sign(err[1]) != np.sign(self.prev_err[1]):
+                self.I_err, self.time_elapsed = (self.I_err[0], 0.0), \
+                                                (self.time_elapsed[0], 0.0)
+
+    def compute_angular_velocity(self, err, derr):
+        '''
+        Compute angular velocity for a PID controller that has constant
+        velocity.
+        '''
+        self.PID = [self.parameters["~k_d"].value * \
+                    np.clip(err[0], -self.parameters["~d_threshold"],
+                                    self.parameters["~d_threshold"]), \
+                    self.parameters["~k_theta"].value * \
+                    np.clip(err[1], -self.parameters["~theta_threshold"],
+                                    self.parameters["~theta_threshold"]), \
+                    self.parameters["~k_Id"].value * self.I_err[0], \
+                    self.parameters["~k_Itheta"].value * self.I_err[1], \
+                    self.parameters["~k_dd"].value * derr[0], \
+                    self.parameters["~k_dtheta"].value * derr[1]]
+
+        return np.clip(sum(self.PID), -np.pi, np.pi)
+
+    def get_parameters_str(self):
+        RET = ""
+        for key in self.parameters.keys():
+            value = self.parameters[key].value \
+                    if isinstance(self.parameters[key], DTParam) \
+                    else self.parameters[key]
+
+            RET += "{}: {}\n".format(key, value)
+        return RET
+
+    def get_PID_str(self):
+        RET = "\tP:\n" + \
+              "\t\td: {:.3f}".format(self.PID[0]) + \
+              "\t\ttheta: {:.3f}\n".format(self.PID[1]) + \
+              "\tI:\n" + \
+              "\t\td: {:.3f}".format(self.PID[2]) + \
+              "\t\ttheta: {:.3f}\n".format(self.PID[3]) + \
+              "\tD:\n" + \
+              "\t\td: {:.3f}".format(self.PID[4]) + \
+              "\t\ttheta: {:.3f}\n".format(self.PID[5])
+        return RET
+
+class SpecialTurningPIDLaneController(DummyLaneController):
+
+    '''
+    SpecialTurningPIDLaneController class has two built-in PID controllers.
+    One is for running in straight lines and one if for doing a turn.
+    '''
+
+    def __init__(self, parameters):
+        # call parent class's __init__ function
+        super(SpecialTurningPIDLaneController, self).__init__(parameters)
+
+        # get turning controller's configuration
+        turning_parameters = self.parameters.pop("~turning_config")
+        self.turn_time_bound = turning_parameters.pop("forced_turn_time")
+        self.half_v_criterion = turning_parameters.pop("half_speed_criterion")
+        self.set_turning_parameters(turning_parameters)
+
+        # forces straight line controller to be a PI controller
+        self.parameters["~k_dd"].set_value(0.0)
+        self.parameters["~k_dtheta"].set_value(0.0)
+
+        # check values for turning controller
+        assert self.turning_parameters["~k_dd"].value <= 0
+        assert self.turning_parameters["~k_dtheta"].value <= 0
+
+        # initialize both controllers
+        self.controllers = {
+                        "straight": BasicPIDLaneController(self.parameters),
+                        "turn": BasicPIDLaneController(self.turning_parameters)
+                        }
+
+        # other settings
+        self.curr_mode = "straight"
+        self.prev_mode = "straight"
+        self.turn_time_elapsed = 0.0
+        self.half_v_flag = False
+        self.prev_v = (self.turning_parameters["~v_forward"], 0.0)
+
+    def set_turning_parameters(self, turning_parameters):
+        '''
+        Set up parameters for the turning controller.
+        '''
+
+        self.turning_parameters = deepcopy(self.parameters)
+
+        for param in turning_parameters.keys():
+            p = "~" + param
+
+            if isinstance(self.turning_parameters[p], DTParam):
+                self.turning_parameters[p].set_value(turning_parameters[param])
+            else:
+                self.turning_parameters[p] = turning_parameters[param]
+
+    def get_car_control(self, **kwargs):
+
+        if not kwargs["in_lane"]:
+            return self.prev_v[0], -self.prev_v[1]
+
+        err = (kwargs["d_err"], kwargs["theta_err"])
+
+        if abs(err[1]) > self.turning_parameters["~theta_resolution"] or \
+           bool(self.turn_time_elapsed > 0.0 and \
+                self.turn_time_elapsed < self.turn_time_bound):
+            # we think we are in a turn
+            self.curr_mode = "turn"
+            if kwargs["dt"]:
+                self.turn_time_elapsed += kwargs["dt"]
+                self.half_v_flag = True
+        else:
+            self.curr_mode = "straight"
+            self.turn_time_elapsed = 0.0
+
+        # if we changed controller, we need to exchange info
+        if self.curr_mode != self.prev_mode:
+            self.controllers[self.curr_mode]._exchange_info(
+                                            self.controllers[self.prev_mode])
+
+        RET = self.controllers[self.curr_mode].get_car_control(**kwargs)
+        if self.curr_mode != self.prev_mode:
+            RET2 = self.controllers[self.prev_mode].get_car_control(**kwargs)
+            RET = 0.65*RET[0]+0.35*RET2[0], 0.65*RET[1]+0.35*RET2[1]
+
+        # if yaw angle is too large, we reduce the speed
+        if abs(err[1]) > self.half_v_criterion:
+            RET = (RET[0]*0.5, RET[1])
+            # self.turn_time_elapsed = sys.float_info.epsilon
+
+        self.prev_mode = self.curr_mode
+        self.prev_v = RET
+
+        return RET
+
+    def get_PID_str(self):
+        return "[{} controller] PID info:\n".format(self.curr_mode.upper()) + \
+                self.controllers[self.curr_mode].get_PID_str()
+
+    def get_parameters_str(self):
+        return "[{} controller] parameters:\n".format(self.curr_mode.upper()) + \
+                self.controllers[self.curr_mode].get_parameters_str()
--- control/exercise_ws/src/lane_control/src/lane_controller_node.py
+++ control/exercise_ws/src/lane_control/src/lane_controller_node.py
@@ -2,25 +2,37 @@
 import numpy as np
 import rospy
 
+# information regarding to DTROS/DTParam can be found in
+# dt-ros-commons/packages/duckietown/include/duckietown/dtros
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
-from duckietown_msgs.msg import Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped, FSMState, StopLineReading
+from duckietown_msgs.msg import Twist2DStamped, LanePose, WheelsCmdStamped, \
+                                BoolStamped, FSMState, StopLineReading
 
-from lane_controller.controller import PurePursuitLaneController
+from lane_controller.controller import DummyLaneController, \
+                                       BasicPIDLaneController, \
+                                       SpecialTurningPIDLaneController
 
 
 class LaneControllerNode(DTROS):
     """Computes control action.
     The node compute the commands in form of linear and angular velocitie.
-    The configuration parameters can be changed dynamically while the node is running via ``rosparam set`` commands.
+    The configuration parameters can be changed dynamically while the node is
+    running via ``rosparam set`` commands.
     Args:
-        node_name (:obj:`str`): a unique, descriptive name for the node that ROS will use
+        node_name (:obj:`str`): a unique, descriptive name for the node that
+        ROS will use
     Configuration:
 
     Publisher:
         ~car_cmd (:obj:`Twist2DStamped`): The computed control action
     Subscribers:
-        ~lane_pose (:obj:`LanePose`): The lane pose estimate from the lane filter
+        ~lane_pose (:obj:`LanePose`): The lane pose estimate from the lane
+        filter
     """
+
+    CONTROLLER_LOOKUP = {"dummy": DummyLaneController,
+                         "basic_pid": BasicPIDLaneController,
+                         "turn_pid": SpecialTurningPIDLaneController}
 
     def __init__(self, node_name):
 
@@ -30,9 +42,100 @@ class LaneControllerNode(DTROS):
             node_type=NodeType.CONTROL
         )
 
-        # Add the node parameters to the parameters dictionary
+        ## Add the node parameters to the parameters dictionary (1) and
+        ## initialize controller (2)
+        # 1. get parameters
         self.params = dict()
-        self.pp_controller = PurePursuitLaneController(self.params)
+        self.params["~v_forward"] = DTParam(
+            "~v_forward",
+            default=0.5,
+            help="forward speed",
+            param_type=ParamType.FLOAT,
+            min_value=0.0,
+            max_value=5.0
+        )
+        self.params["~k_theta"] = DTParam(
+            "~k_theta",
+            default=5.0,
+            help="k_theta's value",
+            param_type=ParamType.FLOAT,
+            min_value=-100.0,
+            max_value=100.0
+        )
+        self.params["~k_d"] = DTParam(
+            "~k_d",
+            help="k_d's value",
+            param_type=ParamType.FLOAT,
+            min_value=-100.0,
+            max_value=100.0
+        )
+        self.params["~k_dd"] = DTParam(
+            "~k_dd",
+            default=5.0,
+            help="k_dd's value",
+            param_type=ParamType.FLOAT,
+            min_value=-100.0,
+            max_value=100.0
+        )
+        self.params["~k_dtheta"] = DTParam(
+            "~k_dtheta",
+            default=5.0,
+            help="k_dtheta's value",
+            param_type=ParamType.FLOAT,
+            min_value=-100.0,
+            max_value=100.0
+        )
+        self.params["~k_Id"] = DTParam(
+            "~k_Id",
+            default=0.0,
+            help="k_Id's value",
+            param_type=ParamType.FLOAT,
+            min_value=-100.0,
+            max_value=100.0
+        )
+        self.params["~k_Itheta"] = DTParam(
+            "~k_Itheta",
+            default=0.0,
+            help="k_Id's value",
+            param_type=ParamType.FLOAT,
+            min_value=-100.0,
+            max_value=100.0
+        )
+        self.params["~theta_threshold"] = rospy.get_param("~theta_threshold",
+                                                          np.pi/6)
+        self.params["~d_threshold"] = rospy.get_param("~d_threshold",
+                                                      0.3)
+        self.params["~k_d_critically_damped_flag"] = rospy.get_param(
+                                                "~k_d_critically_damped_flag",
+                                                False
+                                                )
+        self.params["~d_resolution"] = rospy.get_param("~d_resolution",
+                                                       0.005)
+        self.params["~theta_resolution"] = rospy.get_param("~theta_resolution",
+                                                           0.1)
+        self.params["~d_threshold_robust_flag"] = rospy.get_param(
+                                                "~d_threshold_robust_flag",
+                                                False
+                                                )
+
+        # 2. initialize controller
+        self.controller_type = rospy.get_param("~controller_type", None)
+
+        if self.controller_type == "turn_pid":
+            self.params["~turning_config"] = rospy.get_param("~turning_config")
+
+        assert self.controller_type in \
+               LaneControllerNode.CONTROLLER_LOOKUP.keys()
+        self.controller = LaneControllerNode.CONTROLLER_LOOKUP\
+                            [self.controller_type](self.params)
+
+        # initialize useful parameters
+        self.last_time = None
+        self.fsm_state = None
+        self.wheels_cmd_executed = WheelsCmdStamped()
+        self.stop_line_distance = None
+        self.stop_line_detected = False
+        self.at_stop_line = False
 
         # Construct publishers
         self.pub_car_cmd = rospy.Publisher("~car_cmd",
@@ -45,38 +148,88 @@ class LaneControllerNode(DTROS):
                                                  LanePose,
                                                  self.cbLanePoses,
                                                  queue_size=1)
+        self.sub_intersection_navigation_pose = rospy.Subscriber(
+                                                "~intersection_navigation_pose",
+                                                LanePose,
+                                                self.cbIntersectionPoses,
+                                                queue_size=1
+                                                )
+        self.sub_wheels_cmd_execute = rospy.Subscriber("~wheels_cmd_executed",
+                                                       WheelsCmdStamped,
+                                                       self.cbWheelsCmdExecute,
+                                                       queue_size=1)
+        self.sub_fsm_node = rospy.Subscriber("~fsm_mode",
+                                             FSMState,
+                                             self.cbMode,
+                                             queue_size=1)
+        # self.sub_stop_line = rospy.Subscriber("~stop_line")
 
         self.log("Initialized!")
+        self.log("Lane controller type = {}.".format(self.controller_type))
+
+    def cbMode(self, fsm_state_msg):
+        self.fsm_state = fsm_state_msg.state
+        # self.log("Current fsm state = {}.".format(self.fsm_state))
+
+    def cbIntersectionPoses(self, input_pose_msg):
+        self.cbLanePoses(input_pose_msg)
+
+    def cbWheelsCmdExecute(self, msg_wheels_cmd):
+        self.wheels_cmd_executed = msg_wheels_cmd
 
     def cbLanePoses(self, input_pose_msg):
         """Callback receiving pose messages
 
         Args:
-            input_pose_msg (:obj:`LanePose`): Message containing information about the current lane pose.
+            input_pose_msg (:obj:`LanePose`): Message containing information
+            about the current lane pose.
         """
         self.pose_msg = input_pose_msg
+
+        # get time information
+        duration = None
+        if self.last_time:
+            curr_time = rospy.Time.now().to_sec()
+            duration = curr_time - self.last_time
 
         car_control_msg = Twist2DStamped()
         car_control_msg.header = self.pose_msg.header
 
-        # TODO This needs to get changed
-        car_control_msg.v = 0.5
-        car_control_msg.omega = 0
+        # call controller's get control method to compute the car's next
+        # time step's linear velocity and angular velocity
+        wheels_cmd_flag = [self.wheels_cmd_executed.vel_left,
+                           self.wheels_cmd_executed.vel_right]
+        car_control_msg.v, car_control_msg.omega = \
+            self.controller.get_car_control(d_err=self.pose_msg.d,
+                                            theta_err=self.pose_msg.phi,
+                                            in_lane=self.pose_msg.in_lane,
+                                            wheels_cmd_flag=wheels_cmd_flag,
+                                            dt=duration)
 
         self.publishCmd(car_control_msg)
+
+        # outputs pose information for current time step
+        self.log("\nfsm_state = {}\n".format(self.fsm_state) + \
+                 "\nduration = {}s\n".format(duration) + \
+                 "\npose_msg = \n{}\n".format(self.pose_msg) + \
+                 "\ncar_control_msg = \n{}\n".format(car_control_msg) + \
+                 "\npid values = \n{}\n".format(
+                    self.controller.get_PID_str()))
+        self.last_time = rospy.Time.now().to_sec()
 
 
     def publishCmd(self, car_cmd_msg):
         """Publishes a car command message.
 
         Args:
-            car_cmd_msg (:obj:`Twist2DStamped`): Message containing the requested control action.
+            car_cmd_msg (:obj:`Twist2DStamped`): Message containing the
+            requested control action.
         """
         self.pub_car_cmd.publish(car_cmd_msg)
 
 
     def cbParametersChanged(self):
-        """Updates parameters in the controller object."""
+        """Updates parameters in the controller object. (does not work??)"""
 
         self.controller.update_parameters(self.params)
 
